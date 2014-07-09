9c39d8a39988bd2f0ae094d17bd7fce7d7413cf2
Beatris Boneva <beatrisvboneva@gmail.com>
2014-07-09 12:08:11 +0300

    Adding the currently working stage of the game.

--- artillery.py
+++ artillery.py
@@ -1,8 +1,20 @@
+import re
 from board import Board
 
-board = Board()
-board.draw()
 
-reveal = raw_input()
-if reveal == "Reveal":
-    board.reveal()
+board = Board()
+board.populate_vehicles()
+
+while True:
+    board.draw()
+    cmd = raw_input("Give a shoot: ").lower()
+    coordinate_match = re.match(r"([A-La-l])(\d+)", cmd.upper())
+    if cmd == "reveal":
+        board.draw(reveal=True)
+    elif coordinate_match is not None:
+        column = coordinate_match.groups()[0]
+        row = coordinate_match.groups()[1]
+        if column not in Board.columns or row not in Board.rows:
+            print("Error: bad coordinate")
+            continue
+        board.shoot(cmd.upper())
--- vehicle.py
+++ vehicle.py
@@ -1,31 +1,45 @@
-class Vehicle:
-    def __init__(self, count, size, initials):
-        self.count = count
-        self.size = size
-        self.initials = initials
-    
+class Vehicle(object):
+    def __init__(self):
+        pass
+
     def __str__(self):
         return self.initials
 
 
 class MilitaryTruck(Vehicle):
+    count = 2
+    size = 4
+    initials = 'Mt'
+
     def __init__(self):
-        super(Vehicle, self).__init__(2, 4, 'Mt')
+        super(Vehicle, self).__init__()
 
 
 class Tank(Vehicle):
+    count = 3
+    size = 4
+    initials = 'Tk'
+
     def __init__(self):
-        super(Vehicle, self).__init__(3, 4, 'Tk')
+        super(Vehicle, self).__init__()
 
 
 class Howitzer(Vehicle):
+    count = 4
+    size = 2
+    initials = 'Hr'
+
     def __init__(self):
-        super(Vehicle, self).__init__(4, 2, 'Hr')
+        super(Vehicle, self).__init__()
 
 
 class Jeep(Vehicle):
+    count = 4
+    size = 2
+    initials = 'Jp'
+
     def __init__(self):
-        super(Vehicle, self).__init__(5, 1, 'Jp')
+        super(Vehicle, self).__init__()
 
 
 vehicles = [MilitaryTruck, Tank, Howitzer, Jeep]
